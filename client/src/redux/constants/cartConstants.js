3dc9c2ba83d1296e073f5eccef7b6ac266f40867
Rishika873 <rishupatil0807@gmail.com>
2024-07-18 14:43:23 +0530

    client removed

--- client/src/redux/constants/cartConstants.js
+++
@@ -1,6 +0,0 @@
-
-
-export const ADD_TO_CART = 'addToCart';
-export const ADD_TO_CART_ERROR = 'addtoCartError';
-export const REMOVE_FROM_CART = 'removeFromCart';
-export const CARD_RESET = 'cardReset';
